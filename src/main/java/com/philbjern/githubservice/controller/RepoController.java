2d6fa102c843a9aecb2b1ce0da9ae9f20a9e7e8e
philbjern <filipbiernat@gmail.com>
2024-07-17 22:15:46 +0200

    happy path works

--- src/main/java/com/philbjern/githubservice/controller/RepoController.java
+++ src/main/java/com/philbjern/githubservice/controller/RepoController.java
@@ -1,5 +1,7 @@
 package com.philbjern.githubservice.controller;
 
+import com.philbjern.githubservice.domain.Repository;
+import com.philbjern.githubservice.dto.ErrorDTO;
 import com.philbjern.githubservice.service.GithubAPIService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("api")
@@ -18,9 +23,13 @@ public class RepoController {
     public GithubAPIService githubService;
 
     @GetMapping(path = "/{username}", produces = "application/json")
-    public ResponseEntity<String> getUserReposWithoutForks(@PathVariable String username) {
-        githubService.test(username);
-        return ResponseEntity.ok(username);
+    public ResponseEntity<?> getUserReposWithoutForks(@PathVariable String username) throws IOException {
+        List<Repository> repos = githubService.getUserRepositoriesData(username);
+        if (repos != null) {
+            return ResponseEntity.ok(repos);
+        }
+        ErrorDTO error = new ErrorDTO();
+        return ResponseEntity.notFound().build();
     }
 
 
--- src/main/java/com/philbjern/githubservice/service/GithubAPIService.java
+++ src/main/java/com/philbjern/githubservice/service/GithubAPIService.java
@@ -1,17 +1,87 @@
 package com.philbjern.githubservice.service;
 
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import com.philbjern.githubservice.domain.Branch;
+import com.philbjern.githubservice.domain.Repository;
 import lombok.extern.slf4j.Slf4j;
+import org.kohsuke.github.*;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class GithubAPIService {
 
-    public void test(String username) {
-        log.info("GithubAPIService test {}", username);
+    private GitHub github;
+
+    List<Repository> repos = new ArrayList<>();
+
+    public GithubAPIService() {
+        try {
+            initFromPropertyFile();
+        } catch (IOException e) {
+            log.error("Initialization from property file unsuccessfull");
+        }
+        try {
+            if(github == null) {
+                initFromEnvironemenVariable();
+            }
+        }
+        catch (IOException e) {
+            log.error("Initialization from environment variables unsuccessfull, " +
+                    "try to export GITHUB_OAUTH token variable");
+        }
+    }
+
+    private void initFromEnvironemenVariable() throws IOException {
+        github = GitHubBuilder.fromEnvironment().build();
+    }
+
+    private void initFromPropertyFile() throws IOException {
+        github = GitHubBuilder.fromPropertyFile("src/main/resources/.github").build();
+    }
+
+    public List<Repository> getUserRepositoriesData(String username) throws IOException {
+        GHUser user = github.getUser(username);
+        Map<String, GHRepository> userRepos = user.getRepositories();
+        Map<String, GHRepository> notForkedRepos = getNotForkedRepositories(userRepos);
+
+        notForkedRepos.entrySet().stream().forEach(entry -> {
+            Repository repo = new Repository();
+            repo.setRepositoryName(entry.getKey());
+            repo.setOwnerLogin(entry.getValue().getOwnerName());
+
+            try {
+                Map<String, GHBranch> branches = entry.getValue().getBranches();
+                branches.entrySet().forEach(entry1 -> {
+                    Branch branch = new Branch();
+                    branch.setName(entry1.getKey());
+                    branch.setLastCommitSHA(entry1.getValue().getSHA1());
+                    repo.addBranch(branch);
+                });
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            repos.add(repo);
+        });
+        log.info("GithubAPIService test {}, {}", username, repos);
+        return repos;
+    }
+
+    private Map<String, GHRepository> getNotForkedRepositories(Map<String, GHRepository> repos) {
+        Map<String, GHRepository> notForkedRepos = new HashMap<>();
+        repos.entrySet().stream().forEach(entry -> {
+            String repoName = entry.getKey();
+            if (!entry.getValue().isFork()) {
+                notForkedRepos.put(repoName, entry.getValue());
+            }
+        });
+        return notForkedRepos;
     }
 
 }
