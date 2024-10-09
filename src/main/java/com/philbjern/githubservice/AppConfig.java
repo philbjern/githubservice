98cabb990a161d6d7a8211fde7ed71174aba9d6f
philbjern <filipbiernat@gmail.com>
2024-10-09 11:21:03 +0200

    update AppConfig

--- src/main/java/com/philbjern/githubservice/AppConfig.java
+++ src/main/java/com/philbjern/githubservice/AppConfig.java
@@ -1,6 +1,5 @@
 package com.philbjern.githubservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,16 +8,6 @@ import org.springframework.web.client.RestTemplate;
 @PropertySource("github.properties")
 @Configuration
 public class AppConfig {
-
-//    @Bean
-//    public GithubAPIService githubAPIService() throws IOException {
-//        GithubAPIService github = new GithubAPIService();
-//        github.setup();
-//        return github;
-//    }
-
-    @Value("github.api.key")
-    String myPersonalAccessToken;
 
     @Bean
     public RestTemplate restTemplate() {
