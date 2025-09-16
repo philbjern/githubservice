14f406a5dd836ce7c4f72900f7df35690fdaaa32
philbjern <filipbiernat@gmail.com>
2025-09-16 18:24:37 +0200

    feat: update githubservice to use async api calls

--- src/main/java/com/philbjern/githubservice/controller/RepoController.java
+++ src/main/java/com/philbjern/githubservice/controller/RepoController.java
@@ -7,13 +7,14 @@ import com.philbjern.githubservice.dto.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,58 +23,49 @@ import java.util.List;
 @PropertySource("github.properties")
 public class RepoController {
 
-    private RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public RepoController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public RepoController(WebClient.Builder webClientBuilder,
+                          @Value("${github.api.key}") String githubKey) {
+        this.webClient = webClientBuilder
+                .baseUrl("https://api.github.com")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + githubKey)
+                .build();
     }
-
-    @Value("${github.api.key}")
-    String githubPersonalAccessToken;
 
     @GetMapping(path = "/{username}", produces = "application/json")
-    public ResponseEntity<List<Response>> getUserReposWithoutForks(@PathVariable String username) throws IOException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(githubPersonalAccessToken);
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
+    public Mono<ResponseEntity<List<Response>>> getUserReposWithoutForks(@PathVariable String username) {
+        return webClient.get()
+                .uri("/users/{username}/repos", username)
+                .retrieve()
+                .bodyToFlux(GithubAPIUsersResponse.class)
+                .flatMap(repo -> {
+                    String urlBranches = repo.getBranches_url()
+                            .replace("{/branch}", "");
 
-        String url = "https://api.github.com/users/" + username + "/repos";
-        log.info("Url: {}", url);
-
-        ResponseEntity<List<GithubAPIUsersResponse>> response = restTemplate.exchange(url, HttpMethod.GET, entity,
-                new ParameterizedTypeReference<List<GithubAPIUsersResponse>>() {});
-
-        log.info("Github API Response: {}", response.getBody().get(0).getName());
-
-        List<Response> respList = new ArrayList<>();
-
-        for(GithubAPIUsersResponse repo : response.getBody()) {
-            Response resp = new Response();
-            resp.setRepositoryName(repo.getName());
-            resp.setOwnerLogin(repo.getOwner().getLogin());
-
-            String repoUrlForBranches = repo.getBranches_url();
-            String urlBranches = repoUrlForBranches.substring(0, repoUrlForBranches.length() - "{/branch}".length());
-
-            ResponseEntity<List<Branch>> branchesList = restTemplate.exchange(urlBranches, HttpMethod.GET, entity,
-                    new ParameterizedTypeReference<List<Branch>>() {});
-
-            List<Branch> branches = new ArrayList<>();
-            for (Branch branch : branchesList.getBody()) {
-                log.info("Branch name: {}, last commit SHA: {}", branch.getName(), branch.getCommit().getSha());
-                branches.add(branch);
-            }
-
-            resp.setBranches(branches);
-            respList.add(resp);
-        }
-
-        return ResponseEntity.ok(respList);
+                    return webClient.get()
+                            .uri(urlBranches)
+                            .retrieve()
+                            .bodyToFlux(Branch.class)
+                            .collectList()
+                            .map(branches -> {
+                                Response resp = new Response();
+                                resp.setRepositoryName(repo.getName());
+                                resp.setOwnerLogin(repo.getOwner().getLogin());
+                                resp.setBranches(branches);
+                                return resp;
+                            });
+                })
+                .collectList()
+                .map(ResponseEntity::ok);
     }
 
-    @ExceptionHandler(IOException.class)
-    public ResponseEntity<ErrorDTO> handleIOException(IOException e) {
-        ErrorDTO error = ErrorDTO.builder().errorCode(404).message(e.getMessage()).build();
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleException(IOException e) {
+        ErrorDTO error = ErrorDTO.builder()
+                .errorCode(404)
+                .message(e.getMessage())
+                .build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
